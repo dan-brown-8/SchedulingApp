309ebfcea74a9828cf9436a5efd2ebf8f59db505
Dan Brown <danbrown080@hotmail.com>
2020-07-19 13:34:02 -0400

    Initial Commit

---
+++ SchedulingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SchedulingApp
+//
+//  Created by Dan Brown on 7/19/20.
+//  Copyright © 2020 Dan Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
